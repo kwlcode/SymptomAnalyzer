5e7aa35d6ea522f576b952161f619b8616806427
kwlcode <106566430+kwlcode@users.noreply.github.com>
2026-03-16 18:52:00 +0200

    Fix Drizzle Neon HTTP Serverless Type

--- backend/db/client.ts
+++ backend/db/client.ts
@@ -1,10 +1,7 @@
 import { env } from '../env';
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
-import ws from 'ws';
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from '../db/schema';
 
-neonConfig.webSocketConstructor = ws;
-
-export const pool = new Pool({ connectionString: env.DATABASE_URL });
-export const db = drizzle({ client: pool, schema });
+const sql = neon(env.DATABASE_URL!);
+export const db = drizzle(sql as any, { schema });
